e45ee07fc4b8a305d6bfde265cd41cff574be56b
Guilherme Medeiros <guilherme.medeiros@southsystem.com.br>
2021-06-24 08:53:09 -0300

    Criação de DQL

--- src/br/com/caelum/jms/TesteConsumidorDLQ.java
+++ src/br/com/caelum/jms/TesteConsumidorDLQ.java
@@ -1,42 +1,30 @@
 package br.com.caelum.jms;
 
-import br.com.caelum.modelo.Pedido;
-
 import javax.jms.*;
 import javax.naming.InitialContext;
 import java.util.Scanner;
 
-public class TesteConsumidorTopicoComercial {
+public class TesteConsumidorDLQ {
 
     public static void main(String[] args) throws Exception {
         InitialContext context = new InitialContext();
         ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("ConnectionFactory");
         Connection connection = connectionFactory.createConnection();
-        connection.setClientID("comercial");
         connection.start();
 
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-        Topic topico = (Topic) context.lookup("loja");
-        MessageConsumer consumer = session.createDurableSubscriber(topico, "assinatura");
+        Destination fila = (Destination) context.lookup("DLQ");
+        MessageConsumer consumer = session.createConsumer(fila);
 
         consumer.setMessageListener(message -> {
-            ObjectMessage objectMessage = (ObjectMessage) message;
-            try {
-
-                Pedido pedido = (Pedido) objectMessage.getObject();
-                System.out.println(pedido.getCodigo());
-            } catch (JMSException e) {
-                e.printStackTrace();
-            }
+            System.out.println(message);
         });
 
         new Scanner(System.in).next();
 
-        consumer.close();
         session.close();
         connection.close();
         context.close();
-
     }
 }
